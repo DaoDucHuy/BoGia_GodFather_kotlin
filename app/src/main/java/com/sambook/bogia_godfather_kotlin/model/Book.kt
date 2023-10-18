488804c4f8159ef280c1bb3d9e50730be98f4691
Huy Dao Duc <huydd.1997@gmail.com>
2023-10-18 16:39:47 +0700

    Create MainActivity

---
+++ app/src/main/java/com/sambook/bogia_godfather_kotlin/model/Book.kt
@@ -0,0 +1,19 @@
+package com.sambook.bogia_godfather_kotlin.model
+
+class Book {
+    var id = 0
+    var postter = 0 // anh poster
+
+    var page = 0 // so trang
+
+    var bookShop: String? = null // shop
+
+    var author: String? = null // tac gia
+
+    var content: String? = null // noi dung
+
+    var chapter: String? = null //chương
+
+    var head: String? = null //phần
+
+}
